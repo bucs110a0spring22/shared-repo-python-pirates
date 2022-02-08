5891d9cf84c294c42a1aa67ae4455ba15c34be39
Jdepetr1 P <49722708+jdepetr1@users.noreply.github.com>
2022-02-08 15:38:40 +0000

    Final requirements for exercise

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ mylist.append(inp)
 
 inp = int(input("Input your fourth integer: "))
 mylist.append(inp)
+
+print("\nmylist[0] =",mylist[0])
+print("mylist[1] =",mylist[1])
+print("mylist[2] =",mylist[2])
+print("mylist[3] =",mylist[3],"\n")
+
+mylist[0],mylist[3] = mylist[3],mylist[0]
+
+print(mylist)
