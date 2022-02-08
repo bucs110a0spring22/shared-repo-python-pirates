178f959af5bdc989eea38c1eaaa70531b4756096
Michael O P <98827447+michaelott241@users.noreply.github.com>
2022-02-08 14:56:33 +0000

    Fixed

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ inp = int(input("Input your first integer: "))
 mylist.append(inp)
 
 inp = int(input("Input your second integer: "))
-mylist.append = inp)
+mylist.append(inp)
 
 inp = int(input("Input your third integer: "))
-mylist.append = inp
+mylist.append(inp)
 
 inp = int(input("Input your fourth integer: "))
-mylist.append = inp
+mylist.append(inp)
